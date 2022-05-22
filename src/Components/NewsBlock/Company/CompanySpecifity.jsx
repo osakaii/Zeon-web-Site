31968c4df7b09c2158be887ab7d7a490e536612c
Muhammad <mdzhanov@gmail.com>
2022-05-23 03:55:29 +0600

    added first 3 sections to CompanyPage

--- src/Components/NewsBlock/Company/CompanySpecifity.jsx
+++ src/Components/NewsBlock/Company/CompanySpecifity.jsx
@@ -1,29 +1,44 @@
 import React from "react";
 import styles from "../../../styles/Company/CompanySpecifity.module.scss";
-import image from '../../../Assets/Company/spec.jpeg'
+import image from "../../../Assets/Company/spec.jpeg";
 
 function CompanySpecifity(props) {
   return (
     <section className={styles.Spec}>
       <div className="container">
         <div className={styles.specContent}>
-            <div className={styles.main}>
-               <h1>В чем наша особенность?</h1>
-               <div className={styles.info}>
-                   <p>Мы являемся связующим звеном между компаниями заинтересованными в кадрах 
-                       и талантливыми разработчиками ищущих интересные международные проекты и хорошую зарплату. 
-                    </p>
-                    <div className={styles.stat}>
-                        <div className={styles.statItem}>
-                            <p> <span>$</span>1500</p>
-                            <p></p>
-                        </div>
-                        <div className={styles.statItem}></div>
-                        <div className={styles.statItem}></div>
-                    </div>
-               </div>
+          <div className={styles.main}>
+            <h1>В чем наша особенность?</h1>
+            <div className={styles.info}>
+              <p className={styles.desc}>
+                Мы являемся связующим звеном между компаниями заинтересованными
+                в кадрах и талантливыми разработчиками ищущих интересные
+                международные проекты и хорошую зарплату.
+              </p>
+              <div className={styles.stat}>
+                <div className={styles.statItem}>
+                  <p className={styles.statInfo}>
+                    {" "}
+                    <span>$</span>1500
+                  </p>
+                  <p className={styles.statDesc}>Средняя заработная плата</p>
+                </div>
+                <div className={styles.statItem}>
+                  <p className={styles.statInfo}>20</p>
+                  <p className={styles.statDesc}>Стран</p>
+                </div>
+                <div className={styles.statItem}>
+                  <p className={styles.statInfo}>
+                    145 <span>+</span>
+                  </p>
+                  <p className={styles.statDesc}>
+                    Трудоустроенных разработчиков
+                  </p>
+                </div>
+              </div>
             </div>
-         <img src={image} alt="" />
+          </div>
+          <img src={image} alt="" />
         </div>
       </div>
     </section>
--- src/Pages/Company/CompanyPage.jsx
+++ src/Pages/Company/CompanyPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../../Common/Footer/Footer';
 import Header from '../../Common/Header/Header';
+import CompanyGoals from '../../Components/NewsBlock/Company/CompanyGoals';
 import CompanyIntro from '../../Components/NewsBlock/Company/CompanyIntro';
 import CompanySpecifity from '../../Components/NewsBlock/Company/CompanySpecifity';
 import styles from './CompanyPage.module.scss'
@@ -11,6 +12,7 @@ function CompanyPage({ filled }) {
             <Header filled = { false } />
             <CompanyIntro/>
             <CompanySpecifity/>
+            <CompanyGoals/>
             <Footer/>
         </div>
     );
